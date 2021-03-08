cc4d2594669b8236eb6c1c31f633906f0cda109d
coles427 <coles427@gmail.com>
2021-03-08 12:29:05 -0800

    changed dir control to not use PWM, added a case to backup when line is lost

--- 195b.py
+++ 195b.py
@@ -3,20 +3,23 @@
 # Welcome to the OpenMV IDE! Click on the green run arrow button below to run the script!
 
 import sensor, image, time, pyb
-from pyb import Pin, Timer,LED
-red_led = pyb.LED(1)
-green_led = pyb.LED(2)
-blue_led = pyb.LED(3)
+from pyb import Pin, Timer,LED,USB_VCP
+
 tim4 = Timer(4, freq=300) # Frequency in Hz
 # Generate a 2KHz square wave on TIM4 with 50%, 75% and 50% duty cycles on channels 1, 2 and 3 respectively.
-ld = tim4.channel(2, Timer.PWM, pin=Pin("P8"), pulse_width_percent=0)
+#ld = tim4.channel(2, Timer.PWM, pin=Pin("P8"), pulse_width_percent=0)
+ld = pyb.Pin("P8",pyb.Pin.OUT_PP)
+ld.low()
 lp = tim4.channel(3, Timer.PWM, pin=Pin("P9"), pulse_width_percent=0)
-
+started = -1
+stuck = -1
 tim2 = Timer(2, freq = 300)
-rd = tim2.channel(2, Timer.PWM, pin=Pin("P5"), pulse_width_percent = 0)
+#rd = tim2.channel(2, Timer.PWM, pin=Pin("P5"), pulse_width_percent = 0)
+rd = pyb.Pin("P1", pyb.Pin.OUT_PP)
+rd.low()
 rp = tim2.channel(3, Timer.PWM, pin=Pin("P4"), pulse_width_percent = 0)
 speed=50
-white = [(50, 255)]
+white = [(160, 255)]
 roi1= (0,0,160,10)
 lroi=(0,0,10,120)
 rroi=(150,0,10,120)
@@ -28,9 +31,8 @@ sensor.skip_frames(time = 2000)         # Wait for settings take effect.
 sensor.set_auto_gain(False)             # must be turned off for color tracking
 sensor.set_auto_whitebal(False)         # must be turned off for color tracking
 clock = time.clock()                    # Create a clock object to track the FPS.
-red_led.off()
-green_led.off()
-blue_led.off()
+usb = pyb.USB_VCP() #for debuging when not connected to IDE
+zeroCnt = 0
 while(True):
     clock.tick()                        # Update the FPS clock.
     img = sensor.snapshot()             # Take a picture and return the image.
@@ -61,7 +63,6 @@ while(True):
         right=120-right
     print(midb,left,right)
     if(midb==-1):
-
         if(right==-1 and left==-1):
             move=0
         if right>left:
@@ -74,36 +75,60 @@ while(True):
         move=1
     else:
         move=3
+
+    if stuck == 1:
+        move = -1
+        ld.high()
+        lp.pulse_width_percent(speed)
+        rd.high()
+        rp.pulse_width_percent(speed)
+        print("Stuck, moving back")
+        zeroCnt -= 2
+        if zeroCnt < 1:
+            stuck = 0
     if move==0:
-        ld.pulse_width_percent(0)
+        ld.low()
         lp.pulse_width_percent(0)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(0)
+        zeroCnt += 1
+        if zeroCnt > 10:
+            stuck = 1
     if move==1:
-        ld.pulse_width_percent(0)
+        started = 1
+        ld.low()
         lp.pulse_width_percent(speed)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(speed)
+        zeroCnt = 0
     if move==2:
-        ld.pulse_width_percent(0)
+        started = 1
+        ld.low()
         lp.pulse_width_percent(0)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(speed)
+        zeroCnt = 0
     if move==3:
-        ld.pulse_width_percent(0)
+        started = 1
+        ld.low()
         lp.pulse_width_percent(speed)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(0)
+        zeroCnt = 0
     if move==4:
-        ld.pulse_width_percent(100)
+        started = 1
+        ld.high()
         lp.pulse_width_percent(speed)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(speed)
+        zeroCnt = 0
     if move==5:
-        ld.pulse_width_percent(0)
+        started = 1
+        ld.low()
         lp.pulse_width_percent(speed)
-        rd.pulse_width_percent(0)
+        rd.low()
         rp.pulse_width_percent(0)
+        zeroCnt = 0
     print(move,clock.fps())                  # Note: OpenMV Cam runs about half as fast when connected
                                         # to the IDE. The FPS should increase once disconnected.
 
